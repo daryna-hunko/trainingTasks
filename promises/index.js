b347a801d8c5a1afbe8088a6c62da6c85f615128
dhunko <daryna.hunko@bazaarvoice.com>
2019-12-12 09:59:12 +0200

    updated the promise task one more time

--- promises/index.js
+++ promises/index.js
@@ -1,15 +1,16 @@
 let getResponse = function(url, mode = {mode: 'no-cors'}) {
-  const fetchPromise = fetch(url);
-  fetchPromise.then(response => {
-    checkStatus(response).then(data => console.log(data)).catch(error => console.log(error));
+  return fetch(url)  
+      .then((response) => {return response});
+};
+
+let getAnswer = function(url, mode) {
+  return getResponse(url, mode).then(data => {
+      return new Promise((res, rej) => {
+          if (data.status < 400) res(data);
+          rej('Failed');
+      });
   });
 };
-function checkStatus (data) {
-  return new Promise((res, rej) => {
-    if (data.status < 400) res(data);
-    rej('Failed');
-  });
-}
 
-getResponse('https://dhunko.github.io/trainingTasks');
-getResponse('https://dhunko.github.io/trainingTasks222222');
+getAnswer('https://dhunko.github.io/trainingTasks').then(data => console.log(data)).catch(error => console.log(error));
+getAnswer('https://dhunko.github.io/trainidddddngTasks').then(data => console.log(data)).catch(error => console.log(error));
